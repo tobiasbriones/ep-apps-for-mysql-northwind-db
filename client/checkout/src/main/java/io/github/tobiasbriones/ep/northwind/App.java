9f6bb3b4432bd9bf3ec1b807f823a61842eb1c9f
Tobias Briones <tobiasbrionesdev@gmail.com>
2020-09-25 22:30:46 -0600

    Project created, checkout desktop client

---
+++ client/checkout/src/main/java/io/github/tobiasbriones/ep/northwind/App.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020 Tobias Briones.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package io.github.tobiasbriones.ep.northwind;
+
+public final class App {
+
+    public static void main(final String[] args) {}
+
+    private App() {}
+
+}
---
+++ client/checkout/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module io.github.tobiasbriones.ep.northwind {
+    requires javafx.controls;
+    requires jdk.internal.vm.compiler;
+    exports io.github.tobiasbriones.ep.northwind;
+}
