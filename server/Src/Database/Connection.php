a2897026f462f3b5ba37e0dd7b9ed656021133e9
Tobias Briones <tobiasbrionesdev@gmail.com>
2020-10-16 17:49:13 -0600

    Formatted

--- server/Src/Database/Connection.php
+++ server/Src/Database/Connection.php
@@ -18,7 +18,7 @@ use PDO;
  * @package App\Database
  */
 class Connection {
-    
+
     /**
      * Creates a PDO northwind database connection.
      *
@@ -31,11 +31,11 @@ class Connection {
         $user = Env::get(Env::DB_ROOT_USERNAME_KEY);
         $password = Env::get(Env::DB_ROOT_PASSWORD_KEY);
         $dns = "mysql:host=$host;dbname=$dbname";
-        $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+        $options = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"];
         $conn = new PDO($dns, $user, $password, $options);
-        
+
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
     }
-    
+
 }
