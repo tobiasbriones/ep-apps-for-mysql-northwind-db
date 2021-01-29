59fc57324af3a1b9f7633115286def3d2dde41d9
Tobias Briones <tobiasbrionesdev@gmail.com>
2021-01-29 12:37:19 -0600

    Create Serialization

---
+++ server/Src/Http/Serialization.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Copyright (c) 2021 Tobias Briones.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+namespace App\Http;
+
+class Serialization {
+
+    public const JSON = 1;
+    public const XML = 2;
+    public const HTML = 3;
+
+}
