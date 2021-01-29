cab9c5e926ce55951903c85d11386bacd60b73ba
Tobias Briones <tobiasbrionesdev@gmail.com>
2021-01-29 12:39:48 -0600

    Create AppSerialization

---
+++ server/Src/Config/Http/AppSerialization.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Copyright (c) 2021 Tobias Briones.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+namespace App\Config\Http;
+
+use App\Http\Serialization;
+
+class AppSerialization {
+
+    public const DEF_SERIALIZATION = Serialization::JSON;
+
+}
